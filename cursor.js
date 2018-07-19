b6fbc7b97cb8f57ce6d2e8c3280e0c615fdf1f3f
arisaito <37740514+arisaito@users.noreply.github.com>
2018-07-19 20:01:49 +0900

    add link

--- cursor.js
+++ cursor.js
@@ -1,4 +1,4 @@
-  var boxEl = document.querySelector('Sphere_right1');
+  var boxEl = document.querySelector('#right1');
   // カーソルがぶつかったら拡大
   boxEl.addEventListener('mouseenter', function () {
     boxEl.setAttribute('width', 10);
@@ -9,4 +9,13 @@
   boxEl.addEventListener('mouseleave', function () {
     boxEl.setAttribute('width', 3);
     console.log("mouseleave");
+  });
+
+  let strat = false;
+
+  document.querySelector('#center').addEventListener('click', function (evt) {
+    start = true;
+    if (start) {
+      location.href = "http://arisa-ito.wixsite.com/arisaito";
+    }
   });

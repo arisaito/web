12a68c50565233dd1d0825f3ea7f237a71add9b6
arisaito <37740514+arisaito@users.noreply.github.com>
2018-07-22 22:17:00 +0900

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -13,7 +13,6 @@
 
   let strat = false;
 
- 
 
   document.querySelector('#right1').addEventListener('click', function (evt) {
     start = true;
@@ -25,20 +24,20 @@
   document.querySelector('#left1').addEventListener('click', function (evt) {
     start = true;
     if (start) {
-      location.href = "https://arisaito.github.io/web/";
+      location.href = "http://arisa-ito.wixsite.com/arisaito/woks";
     }
   });
 
   document.querySelector('#right2').addEventListener('click', function (evt) {
     start = true;
     if (start) {
-      location.href = "https://arisaito.github.io/web/";
+      location.href = "http://twitter.com/saharu54";
     }
   });
 
   document.querySelector('#left2').addEventListener('click', function (evt) {
     start = true;
     if (start) {
-      location.href = "https://arisaito.github.io/web/";
+      location.href = "http://arisa-ito.wixsite.com/arisaito/biography";
     }
   });

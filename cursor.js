9bfbc672f17416c529d1d307dd1f8e1a0ed858da
arisaito <saharu54@live.jp>
2019-03-18 11:24:05 +0900

    formatter done

--- cursor.js
+++ cursor.js
@@ -1,37 +1,36 @@
 let strat = false;
 
+document.querySelector("#right1").addEventListener("click", function(evt) {
+  start = true;
+  if (start) {
+    location.href = "http://arisa-ito.wixsite.com/arisaito/products";
+  }
+});
 
-  document.querySelector('#right1').addEventListener('click', function (evt) {
-    start = true;
-    if (start) {
-      location.href = "http://arisa-ito.wixsite.com/arisaito/products";
-    }
-  });
+document.querySelector("#left1").addEventListener("click", function(evt) {
+  start = true;
+  if (start) {
+    location.href = "http://arisa-ito.wixsite.com/arisaito/woks";
+  }
+});
 
-  document.querySelector('#left1').addEventListener('click', function (evt) {
-    start = true;
-    if (start) {
-      location.href = "http://arisa-ito.wixsite.com/arisaito/woks";
-    }
-  });
+document.querySelector("#right2").addEventListener("click", function(evt) {
+  start = true;
+  if (start) {
+    location.href = "http://twitter.com/saharu54";
+  }
+});
 
-  document.querySelector('#right2').addEventListener('click', function (evt) {
-    start = true;
-    if (start) {
-      location.href = "http://twitter.com/saharu54";
-    }
-  });
+document.querySelector("#left2").addEventListener("click", function(evt) {
+  start = true;
+  if (start) {
+    location.href = "https://arisaito.github.io/web/biography";
+  }
+});
 
-  document.querySelector('#left2').addEventListener('click', function (evt) {
-    start = true;
-    if (start) {
-      location.href = "https://arisaito.github.io/web/biography";
-    }
-  });
-
-  document.querySelector('#center').addEventListener('click', function (evt) {
-    start = true;
-    if (start) {
-      location.href = "http://arisa-ito.wixsite.com/arisaito";
-    }
-  });
+document.querySelector("#center").addEventListener("click", function(evt) {
+  start = true;
+  if (start) {
+    location.href = "http://arisa-ito.wixsite.com/arisaito";
+  }
+});

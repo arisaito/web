bd4ab42ba93cd1bb4d4f2e6085047d1c8a3e7491
arisaito <saharu54@live.jp>
2019-04-23 20:28:30 +0900

    contents add

--- 2D/portfolio.js
+++ 2D/portfolio.js
@@ -1,8 +1,51 @@
 $(function () {
-    $('#openModal2').click(function () {
-        $('#modalArea').fadeIn();
+    $('#openModal1').click(function () {
+        $('#modalArea1').fadeIn();
+        $(function scrollFixed() {
+            $('body').css({
+                position: 'fixed',
+                width: '100%'
+            });
+        });
     });
-    $('#closeModal , #modalBg').click(function () {
-        $('#modalArea').fadeOut();
+    $('#closeModal1, #modalBg1').click(function () {
+        $('#modalArea1').fadeOut();
+    });
+});
+
+$(function () {
+    $('#openModal2').click(function () {
+        $('#modalArea2').fadeIn();
+    });
+    $('#closeModal2, #modalBg2').click(function () {
+        $('#modalArea2').fadeOut();
+    });
+});
+
+
+$(function () {
+    $('#openModal3').click(function () {
+        $('#modalArea3').fadeIn();
+    });
+    $('#closeModal3, #modalBg3').click(function () {
+        $('#modalArea3').fadeOut();
+    });
+});
+
+$(function () {
+    $('#openModal4').click(function () {
+        $('#modalArea4').fadeIn();
+    });
+    $('#closeModal4, #modalBg4').click(function () {
+        $('#modalArea4').fadeOut();
+    });
+});
+
+$(function () {
+    $('#openModal5').click(function () {
+        $('#modalArea5').fadeIn();
+    });
+    $('#closeModal5, #modalBg5').click(function () {
+        $('#modalArea5').fadeOut();
     });
 });

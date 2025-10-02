7e4c9b8dc5e402786d25f65e1a93624496314c01
arisa ito <saharu54@live.jp>
2025-10-02 23:38:40 +0900

    fix

--- assets/index-BiDnXvwm.js
+++ assets/index-BiDnXvwm.js
@@ -1,4 +1,4 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))I(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&I(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function I(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const i=`
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))I(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&I(o)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function I(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const s=`
 
        @@BYII=IB
         @AC=---I
@@ -26,4 +26,4 @@
    :@&EYCI==-----=        &@M&AEYYCI==--::
     @#AYCII==---=           @%BAEYYCCIIII=:
     @&EYCI==--=Y            @%BAEEYCII=:
-`,n=()=>{console.log("\x1B[36m%s\x1B[0m",i)};window.addEventListener("DOMContentLoaded",()=>{n()});
+`,i=()=>{console.log("%c"+s,"color: cyan;")};window.addEventListener("DOMContentLoaded",()=>{i()});
